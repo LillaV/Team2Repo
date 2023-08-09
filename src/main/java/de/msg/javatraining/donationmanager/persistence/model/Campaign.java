2e1ff6d8bf9fd6c8b462819a76d6b7fc266daf0e
Noemi Veres <Noemi.Veres@msg.group>
2023-08-09 12:14:03 +0200

    update BE

---
+++ src/main/java/de/msg/javatraining/donationmanager/persistence/model/Campaign.java
@@ -0,0 +1,26 @@
+package de.msg.javatraining.donationmanager.persistence.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "campaign")
+public class Campaign {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String purpose;
+
+
+}
---
+++ src/main/java/de/msg/javatraining/donationmanager/persistence/model/Donation.java
@@ -0,0 +1,40 @@
+package de.msg.javatraining.donationmanager.persistence.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "donation")
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String currency;
+    @Column(nullable = false)
+    private float amount;
+    @Column(nullable = false)
+    private LocalDate createDate;
+    private boolean approved;
+    private LocalDate approvedDate;
+    private String notes;
+    @ManyToOne
+    @JoinColumn(name = "cuser_id")
+    private User createdBy;
+    @ManyToOne
+    @JoinColumn(name = "auser_id")
+    private User approvedBy;
+    @ManyToOne
+    private Campaign campaign;
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "donation")
+    private Set<Donator> benefactor;
+}

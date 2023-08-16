8e9cec51250ec8755f84d802583f3097a03e02f8
Noemi Veres <Noemi.Veres@msg.group>
2023-08-16 09:25:54 +0200

    Delete donation

--- src/main/java/de/msg/javatraining/donationmanager/persistence/factories/DonatorServiceFactory.java
+++ src/main/java/de/msg/javatraining/donationmanager/persistence/factories/DonatorServiceFactory.java
@@ -1,5 +1,6 @@
 package de.msg.javatraining.donationmanager.persistence.factories;
 
+import de.msg.javatraining.donationmanager.persistence.repository.DonationRepository;
 import de.msg.javatraining.donationmanager.persistence.repository.DonatorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Component;
 public class DonatorServiceFactory implements IDonatorServiceFactory{
     @Autowired
     DonatorRepository donatorRepository;
+    @Autowired
+    DonationRepository donationRepository;
     @Override
     public DonatorRepository getDonatorRepository() {
         return donatorRepository;
     }
+    @Override
+    public DonationRepository getDonationRepository() { return donationRepository; }
 }
--- src/main/java/de/msg/javatraining/donationmanager/persistence/repository/NotificationRepository.java
+++ src/main/java/de/msg/javatraining/donationmanager/persistence/repository/NotificationRepository.java
@@ -2,6 +2,8 @@ package de.msg.javatraining.donationmanager.persistence.repository;
 
 import de.msg.javatraining.donationmanager.persistence.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
 }

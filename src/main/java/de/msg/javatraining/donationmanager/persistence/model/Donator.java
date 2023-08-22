80cb4a0960b05ec29686505f10877536b4909023
Victor Baltes <Victor.Baltes@msg.group>
2023-08-22 10:14:36 +0300

    db update

--- src/main/java/de/msg/javatraining/donationmanager/persistence/model/Donator.java
+++ src/main/java/de/msg/javatraining/donationmanager/persistence/model/Donator.java
@@ -25,10 +25,4 @@ public class Donator {
     private String lastName;
     private String additionalName;
     private String maidenName;
-//    @ManyToMany(fetch = FetchType.LAZY)
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "donator_campaign",
-            joinColumns = @JoinColumn(name = "donator_id"),
-            inverseJoinColumns = @JoinColumn(name = "campaign_id"))
-    private Set<Campaign> campaigns = new HashSet<>();
 }

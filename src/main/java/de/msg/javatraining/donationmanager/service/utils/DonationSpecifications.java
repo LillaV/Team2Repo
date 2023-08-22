dc494366c2aef9d2353c66ca3c13f86470f7c79f
Noemi Veres <Noemi.Veres@msg.group>
2023-08-22 08:50:27 +0200

    deleted a System.out

--- src/main/java/de/msg/javatraining/donationmanager/service/utils/DonationSpecifications.java
+++ src/main/java/de/msg/javatraining/donationmanager/service/utils/DonationSpecifications.java
@@ -18,8 +18,6 @@ public class DonationSpecifications {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
 
-            System.out.println(minValue + " " + maxValue);
-
             if (minValue != null && maxValue != null) {
                 predicates.add(criteriaBuilder.between(root.get("amount"), minValue, maxValue));
             } else if (minValue != null) {
